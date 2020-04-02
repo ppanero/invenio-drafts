84c494487e6722362ae6befa50bcce6c61e7b378
Pablo Panero <pablo.panero@cern.ch>
2020-04-02 15:54:40 +0200

    views: bootstrap records, drafts, versions, files and actions

--- invenio_drafts/views.py
+++ invenio_drafts/views.py
@@ -21,9 +21,147 @@ blueprint = Blueprint(
 )
 
 
-@blueprint.route("/")
-def index():
-    """Render a basic view."""
-    return render_template(
-        "invenio_drafts/index.html",
-        module_name=_('Invenio-Drafts'))
+class RecordsListResource(ObjectResource):
+    """RecordsList item resource."""
+
+    def post(self, **kwargs):
+        """Create new draft for new record from nothing."""
+        pass
+
+    def get(self, **kwargs):
+        """Search (published) records."""
+        pass
+
+
+class RecordResource(ObjectResource):
+    """Record item resource."""
+
+    @pass_record
+    def get(self, pid, record, **kwargs):
+    """Get a record."""
+    pass
+
+    @pass_record
+    def delete(self, pid, record, **kwargs):
+    """Delete a record."""
+    pass
+
+
+class DraftsListResource(ObjectResource):
+    """DraftsList item resource."""
+
+    @pass_record
+    def post(self, pid, record, **kwargs):
+        """Create new draft for existing record from existing record."""
+        pass
+
+
+class DraftResource(ObjectResource):
+    """Draft item resource."""
+
+    @pass_record
+    def get(self, pid, record, **kwargs):
+        """Get the latest record version draft.
+
+        Note that a new version represents a new child record. Therefore,
+        this returns a draft of the latest created child.
+        """
+        pass
+
+    @pass_record
+    def put(self, pid, record, **kwargs):
+        """Edit a record draft."""
+        pass
+
+    @pass_record
+    def delete(self, pid, record, **kwargs):
+        """Discard a draft.
+
+        Deletes also the record if it has not been published.
+        """
+        pass
+
+
+class RecordsDraftsMixResource(ObjectResource):
+    """RecordsDraftsMix item resource."""
+
+    def get(self, **kwargs):
+        """Search and display mix of records and drafts."""
+        pass
+
+
+class ActionResource(ObjectResource):
+    """Action item resource."""
+
+    @pass_record
+    @pass_action
+    def post(self, pid, record, action, **kwargs):
+        """Execute the <action> over:
+        - the <record>.
+        - the <draft>.
+
+        POST	/records/:id/actions/:action	execute action
+        POST	/records/:id/draft/actions/publish	Publish draft to record
+        POST	/records/:id/draft/actions/:action	Execute action
+        """
+        pass
+
+
+class VersionsListResource(ObjectResource):
+    """VersionsList item resource."""
+
+    @pass_record
+    def post(self, pid, record, **kwargs):
+        """Create new draft (version) for new record from existing record."""
+        pass
+
+    @pass_record
+    def get(self, pid, record, **kwargs):
+        """Search versions of the record."""
+        pass
+
+
+class VersionResource(ObjectResource):
+    """Version item resource."""
+
+    @pass_record
+    @pass_version
+    def get(self, pid, record, version, **kwargs):
+        """Get a specific version of the record."""
+        pass
+
+
+class FilesListResource(ObjectResource):
+    """FileList item resource."""
+
+    @pass_record
+    def get(self, pid, record, **kwargs):
+        """Get files of the record."""
+        pass
+
+    @pass_record
+    def delete(self, pid, record, **kwargs):
+        """Delete all files from the record."""
+        pass
+
+
+class FileResource(ObjectResource):
+    """File item resource."""
+
+    @pass_record
+    @pass_file
+    def get(self, pid, record, file, **kwargs):
+        """Get a specific file of the record."""
+        pass
+
+    @pass_record
+    @pass_file
+    def put(self, pid, record, file, **kwargs):
+        """Upload a file to the record."""
+        pass
+
+    @pass_record
+    @pass_file
+    def delete(self, pid, record, file, **kwargs):
+        """Delete a file from the record."""
+        pass
